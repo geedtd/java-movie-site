48b9407dc2032ef2ba6746008f4518728981cb08
geedtd <grcazares@gmail.com>
2023-04-17 18:51:40 -0700

    working on ReviewController

--- src/main/java/dev/gcazares/movies/Review.java
+++ src/main/java/dev/gcazares/movies/Review.java
@@ -19,4 +19,8 @@ public class Review {
     private ObjectId id;
 
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
--- src/main/java/dev/gcazares/movies/ReviewRepository.java
+++ src/main/java/dev/gcazares/movies/ReviewRepository.java
@@ -1,8 +1,10 @@
 package dev.gcazares.movies;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import org.bson.types.ObjectId;
 
+@Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
     
 }
